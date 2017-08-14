95c967655553c93c6400e41d1cf5c7ce4ceec7fd
bindu0903 <30993650+bindu0903@users.noreply.github.com>
2017-08-13 23:44:46 -0400

    Add files via upload

---
+++ Utils.Twitter/TwitterHelperExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Utils.Twitter
+{
+    public static class TwitterHelperExtensions
+    {
+        public static string Join<T>(this IEnumerable<T> items, string separator)
+        {
+            return string.Join(separator, items.ToArray());
+        }
+
+        public static IEnumerable<T> Concat<T>(this IEnumerable<T> items, T value)
+        {
+            return items.Concat(new[] { value });
+        }
+
+        public static string EncodeRFC3986(this string value)
+        {
+            // From Twitterizer http://www.twitterizer.net/
+
+            if (string.IsNullOrEmpty(value))
+                return string.Empty;
+
+            var encoded = Uri.EscapeDataString(value);
+
+            return Regex
+                .Replace(encoded, "(%[0-9a-f][0-9a-f])", c => c.Value.ToUpper())
+                .Replace("(", "%28")
+                .Replace(")", "%29")
+                .Replace("$", "%24")
+                .Replace("!", "%21")
+                .Replace("*", "%2A")
+                .Replace("'", "%27")
+                .Replace("%7E", "~");
+        }
+    }
+}
