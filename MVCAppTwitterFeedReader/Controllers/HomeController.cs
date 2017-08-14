30086222f61f9fc93e85f79dd2264ceabb86e543
bindu0903 <30993650+bindu0903@users.noreply.github.com>
2017-08-13 23:34:51 -0400

    Add files via upload

--- MVCAppTwitterFeedReader/Controllers/HomeController.cs
+++ MVCAppTwitterFeedReader/Controllers/HomeController.cs
@@ -1,1 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Utils.Twitter;
+using Utils.Twitter.Models;
 
+namespace MVCAppTwitterFeedReader.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            var twitter = new TwitterHelper();
+            var tweets = twitter.GetUserTimeline(null, null, 10, "SalesForce");
+            ViewBag.Tweets = tweets;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
