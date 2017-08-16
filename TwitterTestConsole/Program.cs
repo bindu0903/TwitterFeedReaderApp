ece15d6ac07b33a7921a897b48924c2407213520
bindu0903 <30993650+bindu0903@users.noreply.github.com>
2017-08-16 17:34:44 -0400

    Add files via upload

--- TwitterTestConsole/Program.cs
+++ TwitterTestConsole/Program.cs
@@ -1,1 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utils.Twitter;
 
+namespace TwitterTestConsole
+{
+    public class Program
+    {
+        public static string ConsumerKey = ConfigurationManager.AppSettings["ConsumerKey"];
+        public static string ConsumerSecret = ConfigurationManager.AppSettings["ConsumerSecret"];
+        public static string AccessToken = ConfigurationManager.AppSettings["AccessToken"];
+        public static string AccessTokenSecret = ConfigurationManager.AppSettings["AccessTokenSecret"];
+
+
+        static void Main(string[] args)
+        {
+            var twitter = new TwitterHelper();
+            var tweets = twitter.GetUserTimeline(null, null, 20, "SalesForce");
+            foreach (var tweet in tweets)
+            {
+                Console.WriteLine(string.Format(@"{0} alias {1} posted tweet {2}: {3}", tweet.UserName, tweet.ScreenName, tweet.Id, tweet.Text));
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
